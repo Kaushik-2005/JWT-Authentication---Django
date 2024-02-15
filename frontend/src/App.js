528b24fafc686fd503078d77ff7323618732caa2
K4u5hik <kaushik.kesanapalli@gmail.com>
2024-02-16 02:41:12 +0530

    set up frontend react

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage  from './pages/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello</p>
+      <Router>
+        <Routes>
+          <Route element={<HomePage/>} path='/'/>
+          <Route element={<LoginPage/>} path='/login' />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
