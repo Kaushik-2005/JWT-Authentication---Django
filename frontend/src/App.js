83112c190bdfcdad0a48970d9a6955ed5f3ee49d
K4u5hik <kaushik.kesanapalli@gmail.com>
2024-02-20 22:20:07 +0530

    set up header.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage  from './pages/LoginPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
       <Router>
+        <Header/>
         <Routes>
           <Route element={<HomePage/>} path='/'/>
           <Route element={<LoginPage/>} path='/login' />
